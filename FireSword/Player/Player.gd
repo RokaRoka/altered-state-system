3b6182d9310422641954606040d8b5a12c6e20b4
samscott <samuel.scott@mymail.champlain.edu>
2018-04-06 23:20:10 -0400

    Initial player movement

--- FireSword/Player/Player.gd
+++ FireSword/Player/Player.gd
@@ -1,16 +1,11 @@
 extends KinematicBody
 
-#position constants
-const VEC3_FORWARD = Vector3( 0, 0, 1 )
-const VEC3_LEFT = Vector3( 1, 0, 0 )
-const VEC3_UP = Vector3( 0, 1, 0 )
-
 #physics constants
 const GRAVITY = Vector3(0, -30, 0)
 
 #editor members
 #player speed in pixels per second
-export var player_spd = 2
+export var player_spd = 128
 
 #private members
 var buffered_velocity = Vector3()
@@ -19,37 +14,26 @@ var input_direction = Vector3()
 func _ready():
 	pass
 
-func _input(event):
-	if event is InputEventKey:
-		match event.scancode:
-			KEY_W:
-				#move forward
-				input_direction += (VEC3_FORWARD * player_spd)
-			KEY_A:
-				#move left
-				input_direction += ( VEC3_LEFT * player_spd)
-			KEY_D:
-				#move right
-				input_direction += ( VEC3_LEFT * -1 * player_spd)
-			KEY_S:
-				#move back
-				input_direction += ( VEC3_FORWARD * -1 * player_spd)
-		if event.pressed:
-			bufferMovement( input_direction )
-		else:
-			#bufferMovement( input_direction * -1)
-			pass
-
 func _physics_process(delta):
-	playerMovement()
+	input_direction = Vector3()
+	bufferMovement()
+	playerMovement(delta)
 	#applyGravity()
 
-func playerMovement():
-	if buffered_velocity.length_squared() > 0:
-		move_and_slide( buffered_velocity )
 
-func bufferMovement(direction):
-	buffered_velocity = direction * player_spd
+func bufferMovement():
+	if Input.is_action_pressed( "player_up" ):
+		input_direction += (dir.forward)
+	if Input.is_action_pressed( "player_left" ):
+		input_direction += (dir.left)
+	if Input.is_action_pressed( "player_right" ):
+		input_direction += ( dir.left * -1)
+	if Input.is_action_pressed( "player_down" ):
+		input_direction += ( dir.forward * -1)
+
+func playerMovement(delta):
+	if input_direction.length_squared() > 0:
+		move_and_slide( input_direction * player_spd * delta)
 
 func applyGravity():
 	var coll = move_and_collide( GRAVITY )
---
+++ FireSword/System/directions.gd
@@ -0,0 +1,7 @@
+extends Node
+
+const center = Vector3()
+
+const left = Vector3(1, 0, 0)
+const forward = Vector3( 0, 0, 1)
+const up = Vector3( 0, 1, 0)
